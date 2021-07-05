c223958771b21a42cce281af54e6bbb79c892144
EroAuditore <alfa.fenrir@gmail.com>
2021-07-05 13:29:27 -0500

    Add button with display in the dom

--- assets/script.js
+++ assets/script.js
@@ -1,66 +1,38 @@
 //Library Script
 
 let library = [];
-let book = {};
-let book1 = {};
 
-const addBook = (book) => {
+
+const addBook = () => {
+    let book = {};
+    book.author = document.getElementById('Author').value;;
+    book.title = document.getElementById('Title').value;
     library.push(book);
+    displayBooks();
 }
 const removeBook = (bookRemove) => {
     library = library.filter((book) => book.title !== bookRemove.title);
 }
 
+const displayBooks= ()=>{
 
-book.author = "JK Robin";
-book.title = "Harry Potter";
-addBook(book);
+   
+    libraryDiv = document.getElementById('library');
+    libraryDiv.innerHTML = "";
+    library.map((book)=>{
+        divBook = document.createElement('div')
+        p = document.createElement('p') //Author
+        p.innerHTML = book.author;
+        p2= document.createElement('p') //Book
+        p2.innerHTML = book.title;
+        hr = document.createElement('hr')
+        btn = document.createElement('BUTTON');
+        btn.innerHTML = "Remove"
+        divBook.appendChild(p);
+        divBook.appendChild(p2);
+        divBook.appendChild(hr);
+        divBook.appendChild(btn);
+        libraryDiv.appendChild(divBook);
+    })
 
-book1.author = "Robin";
-book1.title = "Harry";
-addBook(book1);
-
-
-
-
-libraryDiv = document.getElementById('library');
-
-// Book 1
-divBook = document.createElement('div')
-p = document.createElement('p') //Author
-p.innerHTML = book.author;
-p2= document.createElement('p') //Book
-p2.innerHTML = book.title;
-hr = document.createElement('hr')
-btn = document.createElement('BUTTON');
-btn.innerHTML = "Remove"
-
-divBook.appendChild(p);
-divBook.appendChild(p2);
-divBook.appendChild(hr);
-divBook.appendChild(btn);
-
-
-libraryDiv.appendChild(divBook);
-
-
-// Book 2
-divBook = document.createElement('div')
-p = document.createElement('p') //Author
-p.innerHTML = book1.author;
-p2= document.createElement('p') //Book
-p2.innerHTML = book1.title;
-hr = document.createElement('hr')
-btn = document.createElement('BUTTON');
-btn.innerHTML = "Remove"
-
-divBook.appendChild(p);
-divBook.appendChild(p2);
-divBook.appendChild(hr);
-divBook.appendChild(btn);
-
-libraryDiv.appendChild(divBook);
-
-
-
-console.log(libraryDiv);
+};
