5247a27fc9f9ec0815edd10cc1d596c26b576f4b
Carlos-SSH <carlos.anezeh@me.com>
2021-07-05 12:46:51 -0500

    Create a function add.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,20 @@
 //Library Script
+
+let library = [];
+let book = {};
+let book1 = {};
+
+const addBook = (book) => {
+    library.push(book);
+}
+
+
+book.author = "JK Robin";
+book.title = "Harry Potter";
+addBook(book);
+
+book1.author = "Robin";
+book1.title = "Harry";
+addBook(book1);
+
+console.log(library);
